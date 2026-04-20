83b6e0cc6a3a9df7316dfc0b557229fed927f40c
abir781 <hasan.abir3176@gmail.com>
2026-04-20 06:52:42 +0600

    kdjfj

--- red.c
+++ red.c
@@ -409,3 +409,27 @@ int main()
 }
 return 0;
 }
+
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    scanf("%d %d %d %d", &a,&b,&c, &d);
+    
+    int result1=a+b-c;
+    int result2=a+b*c;
+    int result3=a-b+c;
+    int result4=a*b+c;
+    int result5=a*b-c;
+    int result6=a-b*c;
+    
+    if(result1==d || result2==d || result3==d || result4==d || result5==d || result6==d){
+        printf("YES");
+    }
+    else{
+        printf("NO");
+    }
+
+    return 0;
+}
